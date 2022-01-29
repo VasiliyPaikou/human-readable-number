b51b7afa5f2d2c27df1d7082d8c5b532fd6d1cce
VasiliyPaikou <gtr208111@gmail.com>
2022-01-29 20:32:18 +0300

    feat: nice task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const arrZN = ['^', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    const arrTenNineteen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    const arrTwentyNinety = ['^', '^', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    if (number === 0) {
+        return 'zero'
+    }
+    else if (number.toString().length === 1) {
+        return arrZN[number.toString()[0]]
+    } else if (number.toString().length === 2) {
+        if (number.toString()[0] === '1') {
+            return arrTenNineteen[number.toString()[1]]
+        } else {
+            if (number.toString()[1] === '0') {
+                return arrTwentyNinety[number.toString()[0]]
+            }
+            return `${arrTwentyNinety[number.toString()[0]]} ${arrZN[number.toString()[1]]}`
+        }
+    } else if (number.toString().length === 3) {
+        if (number.toString()[1] === '1') {
+            return `${arrZN[number.toString()[0]]} hundred ${arrTenNineteen[number.toString()[2]]}`
+        } else if (number.toString()[1] === '0' && number.toString()[2] !== '0') {
+            return `${arrZN[number.toString()[0]]} hundred ${arrZN[number.toString()[2]]}`
+        } 
+        else if (number.toString()[1] === '0' && number.toString()[2] === '0') {
+            return `${arrZN[number.toString()[0]]} hundred`
+        } 
+        else if (number.toString()[2] === '0') {
+            return `${arrZN[number.toString()[0]]} hundred ${arrTwentyNinety[number.toString()[1]]}`
+        } else {
+            return `${arrZN[number.toString()[0]]} hundred ${arrTwentyNinety[number.toString()[1]]} ${arrZN[number.toString()[2]]}`
+        }
+    }
+    
 }
